3936adb71e1266725ce92dcfcab6a627ad62d403
Марат Замалетдинов <148064047+Zamlet97@users.noreply.github.com>
2024-07-06 14:19:27 +0400

    добавления prettier

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,22 @@
-function countMaxString (line, number) {
-  return (line.length <= number);
+// function countMaxString (line, number) {
+//   return (line.length <= number);
+// }
+
+// countMaxString();
+
+// function checksStringPalin (line) {
+//   const string = line.replaceAll(' ', '').toLowerCase();
+//   let empty = '';
+
+//   for (let ind = string.length - 1; ind >= 0; ind --){
+//     empty += string[ind];
+//   }
+//   return string === empty;
+// }
+
+// //schecksStringPalin();
+if (true) {
+
+
+  console.log('hello!');
 }
-
-countMaxString();
-
-function checksStringPalin (line) {
-  const string = line.replaceAll(' ', '').toLowerCase();
-  let empty = '';
-
-  for (let ind = string.length - 1; ind >= 0; ind --){
-    empty += string[ind];
-  }
-  return string === empty;
-}
-
-//schecksStringPalin();
