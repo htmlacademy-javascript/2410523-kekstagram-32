4f7b4a8594f42df98e839cc325fa17b58fa2855f
marat <marat.zamlet-97@yandex.ru>
2024-07-10 17:49:50 +0400

    Работаю над 5.2

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,29 @@
-function countMaxString (line, number) {
-  return (line.length <= number);
-}
+// function countMaxString (line, number) {
+//   return (line.length <= number);
+// }
 
-countMaxString();
+// countMaxString();
 
-function checksStringPalin (line) {
-  const string = line.replaceAll(' ', '').toLowerCase();
-  let empty = '';
+// function checksStringPalin (line) {
+//   const string = line.replaceAll(' ', '').toLowerCase();
+//   let empty = '';
 
-  for (let ind = string.length - 1; ind >= 0; ind --){
-    empty += string[ind];
+//   for (let ind = string.length - 1; ind >= 0; ind --){
+//     empty += string[ind];
+//   }
+//   return string === empty;
+// }
+
+// checksStringPalin();
+
+
+const getTime = (time) => {
+  let emptyTime = [ ];
+  for (let i = 0; i <= time.length - 1; i++) {
+    emptyTime += time[i];
+    return emptyTime;
   }
-  return string === empty;
-}
+  return emptyTime;
+};
 
-checksStringPalin();
+console.log(getTime(34,11));
