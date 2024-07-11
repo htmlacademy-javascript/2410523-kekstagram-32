86c54fcabb813d928ee1fa9ac4839e85b871fca0
Марат Замалетдинов <148064047+Zamlet97@users.noreply.github.com>
2024-07-11 16:23:51 +0400

    Module5-task2 (#5)
    * Раьоб над 5.2
    
    * исправляю ошибки для 5.2
    
    * Работаю над 5.2
    
    * Сделал задание 5.2

--- js/functions.js
+++ js/functions.js
@@ -15,3 +15,32 @@ function checksStringPalin (line) {
 }
 
 checksStringPalin();
+
+function isMeetingWithinWorkingHours(startTime, endTime, meetingStartTime, duration) {
+  const startTimeParts = startTime.split(':');
+  const endTimeParts = endTime.split(':');
+
+  const startTimeInMinutes = parseInt(startTimeParts[0], 10) * 60 + parseInt(startTimeParts[1], 10);
+  const endTimeInMinutes = parseInt(endTimeParts[0], 10) * 60 + parseInt(endTimeParts[1], 10);
+
+  const meetingStartTimeInMinutes = parseInt(meetingStartTime.split(':')[0], 10) * 60 + parseInt(meetingStartTime.split(':')[1], 10);
+
+  if (startTimeInMinutes >= endTimeInMinutes) {
+    return false;
+  } else {
+
+    if (meetingStartTimeInMinutes < startTimeInMinutes || meetingStartTimeInMinutes > endTimeInMinutes) {
+      return false;
+    }
+
+    const totalDurationInMinutes = duration + meetingStartTimeInMinutes;
+
+    if (totalDurationInMinutes <= endTimeInMinutes && totalDurationInMinutes >= startTimeInMinutes) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+console.log(isMeetingWithinWorkingHours('08:00', '17:30', '14:00', 90));
