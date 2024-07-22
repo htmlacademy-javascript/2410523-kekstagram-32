59bc1b9ed55ab13330db9ca9b69687d510e3a433
marat <marat.zamlet-97@yandex.ru>
2024-07-22 17:40:09 +0400

    Работаю над 8.1

---
+++ js/bigPicture.js
@@ -0,0 +1,13 @@
+import {generateMiniatures} from './miniatures.js';
+
+const bigPicture = document.querySelector('.big-picture');
+
+bigPicture.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  bigPicture.classList.remove('hidden');
+  const createBigImage = () => {
+
+
+  };
+});
+
