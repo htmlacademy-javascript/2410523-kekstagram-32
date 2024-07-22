18bbbe754cb3716fa368fc851794d617d2e0fdb9
Марат Замалетдинов <148064047+Zamlet97@users.noreply.github.com>
2024-07-22 21:12:11 +0400

    работаю над 8.1

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,13 +1,24 @@
 import {generateMiniatures} from './miniatures.js';
 
 const bigPicture = document.querySelector('.big-picture');
+const bigPictureItem = document.querySelector('.big-picture__img');
+const likesUsers = document.querySelector('.likes-count');
+const numberComments = document.querySelector('.social__comment-show-count');
+const socialComments = document.querySelector('.social__comments');
 
-bigPicture.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  bigPicture.classList.remove('hidden');
-  const createBigImage = () => {
 
+const createBigPicture = () => {
+  bigPicture.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    bigPicture.classList.remove('hidden');
+    generateMiniatures();
+  // const createBigImage = (picture) => {
+  //   bigPictureItem.src = picture.url;
+  //   likesUsers.textContent = picture.likes;
+  //   numberComments.textContent = picture.comments;
+  //   socialComments;
+  }
+  );
+};
 
-  };
-});
-
+export {createBigPicture};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import {getUsersPhoto} from './data.js';
 import {generateMiniatures} from './miniatures.js';
+import { createBigPicture } from './bigPicture.js';
 
 generateMiniatures(getUsersPhoto());
+createBigPicture();
