b24b85459afcde3740fc01802158a48e01a3742d
Марат Замалетдинов <148064047+Zamlet97@users.noreply.github.com>
2024-07-30 22:57:36 +0400

    Работаю над 9 задание ч 2

--- js/uploaf-form-modal.js
+++ js/uploaf-form-modal.js
@@ -1,3 +1,32 @@
 import { isEscapeKey } from './utils';
 
-const
+const uploadInputElement = document.querySelector('.img-upload__input');
+const uploadOverlayElement = document.querySelector('.img-upload__overlay');
+const buttonCloseUploadElement = document.querySelector('.img-upload__cancel');
+
+
+//обработчик закрытия Esс
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    // eslint-disable-next-line no-use-before-define
+    getCloseLoad();
+  }
+};
+
+const getLoadImage = () => {
+  uploadInputElement.addEventListener('click', () => {
+    uploadOverlayElement.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+  });
+};
+
+const getCloseLoad = () => {
+  buttonCloseUploadElement.addEventListener('click', () => {
+    uploadOverlayElement.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    document.removeEventListener('keydown', onDocumentKeydown);
+  });
+};
+
+export {getLoadImage, getCloseLoad};
