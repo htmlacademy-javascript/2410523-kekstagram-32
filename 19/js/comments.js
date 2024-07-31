1c81a24c41341260ca12b3b39eb855bf0ac1d891
Keks <keks@htmlacademy.ru>
2024-07-31 18:05:51 +0300

    :heavy_check_mark: Сборка #19

---
+++ 19/js/comments.js
@@ -0,0 +1,50 @@
+const socialListComments = document.querySelector('.social__comments');
+const socialCommentElement = document.querySelector('.social__comment');
+const commentsLoadElement = document.querySelector('.comments-loader');
+const commetsShownElement = document.querySelector('.social__comment-shown-count');
+const commentsTotalElement = document.querySelector('.social__comment-total-count');
+
+const NEXT_NUBERS_QTY = 5;
+let numbersComments = [];
+let startIndex = 0;
+
+
+//создание 1 комента
+const getUserComment = ({avatar, name, message }) => {
+  const itemComment = socialCommentElement.cloneNode(true);
+  const socialPictureElement = itemComment.querySelector('.social__picture');
+  socialPictureElement.src = avatar;
+  socialPictureElement.alt = name;
+  itemComment.querySelector('.social__text').textContent = message;
+
+  return itemComment;
+};
+
+//  загрузка по 5 коментариев.
+const getLoadComments = () => {
+  const endIndex = Math.min(startIndex + NEXT_NUBERS_QTY, numbersComments.length);
+  commetsShownElement.textContent = endIndex;
+
+  numbersComments.slice(startIndex, endIndex).forEach((element) => {
+    const listComment = getUserComment(element);
+    socialListComments.append(listComment);
+  });
+  startIndex += NEXT_NUBERS_QTY;
+  if (endIndex >= numbersComments.length) {
+    commentsLoadElement.classList.add('hidden');
+  }
+};
+
+//создание списка коментариев
+const getListComments = (element) => {
+  startIndex = 0;
+  numbersComments = element;
+  socialListComments.innerHTML = '';
+  commentsLoadElement.classList.remove('hidden');
+  commentsTotalElement.textContent = element.length;
+  getLoadComments();
+  //Добавляем обработчик на открытие 5 след коментариев
+  commentsLoadElement.addEventListener('click', getLoadComments);
+};
+
+export { getListComments };
