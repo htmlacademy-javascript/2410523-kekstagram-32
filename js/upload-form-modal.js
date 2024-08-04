f4397d00848da3e30f23b9966a1bf639183cf645
Марат Замалетдинов <148064047+Zamlet97@users.noreply.github.com>
2024-08-04 17:30:01 +0400

    Закончил дз 9.15

--- js/upload-form-modal.js
+++ js/upload-form-modal.js
@@ -1,3 +1,6 @@
+import { initEffectPicture, resetEffectPicture } from './effect-picture';
+import { resetScalePicture } from './scale-picture';
+
 const body = document.querySelector('body');
 const form = document.querySelector('.img-upload__form');
 const uploadInputElement = form.querySelector('.img-upload__input');
@@ -30,6 +33,8 @@ const getLoadImage = () => {
 const getCloseLoad = () => {
   pristine.reset();
   form.reset();
+  resetScalePicture();
+  resetEffectPicture();
   uploadOverlayElement.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
@@ -103,4 +108,4 @@ function onDocumentKeydown (evt) {
 uploadInputElement.addEventListener('change', onPopupOpen);
 buttonCloseUploadElement.addEventListener('click', onPopupClose);
 form.addEventListener('submit', onFormSubmit);
-
+initEffectPicture();
