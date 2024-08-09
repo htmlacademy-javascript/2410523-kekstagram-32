a2be9b729a27fb9cf0765f492e695c15cc86c24b
Марат Замалетдинов <148064047+Zamlet97@users.noreply.github.com>
2024-08-09 11:39:27 +0400

    Module9-task2 (#14)
    * Сделал дз 7
    
    * Работаю над 8.1
    
    * работаю над 8.1
    
    * Merge pull request #15 from Zamlet97/module7-task1
    
    * Работаю над 8.1 ч3
    
    * зашел  в тупик 8.1
    
    * Затык с заданием 8.14
    
    * Исправления после созвона
    
    * Исправил ошибки  и работаю над списками комент
    
    * сделал коменты 8.15
    
    * Отправляю на провекру 8.14 и 8.15
    
    * Fixed 8.15
    
    * Merge branch 'master' into module8-task1
    
    * fixed fixed 5.15
    
    * Merge pull request #17 from Zamlet97/module8-task1
    
    * 123
    
    * 8.15
    
    * Merge pull request #18 from Zamlet97/module8-task2
    
    * работаю над 9 дз
    
    * Работаю над 9 задание ч 2
    
    * Ищу ошибку в 9.1
    
    * Fix 9/1
    
    * Закончил с модулем 9.1
    
    * Merge pull request #19 from Zamlet97/module9-task1
    
    * Опять конфликты
    
    * Исправил конфликты
    
    * 3213123
    
    * Закончил дз 9.15
    
    * Merge branch 'master' into module9-task2
    
    * Работаю над 11 модулем
    
    * Fix 11.14
    
    * Сделал модуль 11
    
    * Merge branch 'master' into module9-task2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 10cd37f (fixed fixed 5.15)
 import {photos} from './data.js';
 import {generateMiniatures} from './miniatures.js';
 import {generateBigPicture} from'./big-picture.js';
-import './upload-form-modal.js';
 
 generateMiniatures(photos);
 generateBigPicture(photos);
+<<<<<<< HEAD
+=======
+import {generatePhoto} from './data.js';
+import {generateMiniatures} from './miniatures.js';
+import {generateBigPicture} from'./big-picture.js';
+
+generateMiniatures(generatePhoto);
+<<<<<<< HEAD
+
+>>>>>>> 61e95ab (Работаю над 8.1 ч3)
+=======
+generateBigPicture(generatePhoto);
+>>>>>>> 4e45a33 (Затык с заданием 8.14)
+=======
+import {photos} from './data.js';
+import {generateMiniatures} from './miniatures.js';
+import {generateBigPicture} from'./big-picture.js';
+
+generateMiniatures(photos);
+generateBigPicture(photos);
+>>>>>>> b9891b7 (Исправления после созвона)
+=======
+import {getUsersPhoto} from './data.js';
+import {generateMiniatures} from './miniatures.js';
+
+generateMiniatures(getUsersPhoto());
+>>>>>>> f7b2044 (Сделал дз 7)
+=======
+>>>>>>> 10cd37f (fixed fixed 5.15)
--- js/utils.js
+++ js/utils.js
@@ -1,20 +1,13 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const ALERT_SHOW_TIME = 5000;
+const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+const showAlert = () => {
+  const dataErrorElement = dataErrorTemplate.cloneNode(true);
+  document.body.append(dataErrorElement);
+  setTimeout(() => {
+    dataErrorElement.remove();
+  }, ALERT_SHOW_TIME);
 };
-
-const usersIdIndex = () => {
-  let id = 1;
-  return () => {
-    id ++;
-    return id;
-  };
-};
-
-const randomIdIndex = usersIdIndex();
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 
-export {getRandomInteger, randomIdIndex, isEscapeKey};
+export {showAlert, isEscapeKey};
