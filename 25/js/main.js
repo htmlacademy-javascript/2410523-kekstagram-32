0e80f77b77191f021680fcb0b91ad09b8c659a3c
Keks <keks@htmlacademy.ru>
2024-08-09 15:33:06 +0300

    :heavy_check_mark: Сборка #25

---
+++ 25/js/main.js
@@ -0,0 +1,28 @@
+import {generateMiniatures} from './miniatures.js';
+import {generateBigPicture} from'./big-picture.js';
+import {setOnFormSubmit, getCloseLoad} from './upload-form-modal.js';
+import {getData, sendData} from './api.js';
+import {showAlert, debounce} from './utils.js';
+import {showSuccessMessage, showErrorMessage} from './message.js';
+import { initFilters, getFilteredPictures } from './filters-picture.js';
+
+//Получение данных из сервера
+setOnFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    getCloseLoad();
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+  }
+});
+try {
+  const data = await getData();
+  const debounceGenerateThumbnails = debounce(generateMiniatures);
+  initFilters(data, debounceGenerateThumbnails);
+  generateMiniatures(getFilteredPictures());
+  generateMiniatures(data);
+  generateBigPicture(data);
+} catch {
+  showAlert();
+}
